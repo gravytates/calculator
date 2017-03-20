d0ab1b0c50a71cd7873324ffe72239b9c0674d98
Grady Shelton and Sara Schultz <sara@>
2017-03-20 13:14:45 -0700

    add bmi calculator and fahrenheit to celsius converter to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,52 @@ var result = multiply(number1, number2);
 var result = division(number1, number2);
 
 alert(result);
+
+
+// BMI CALCULATOR
+
+var lbs = parseInt(prompt("What is your weight in pounds?"));
+var inches = parseInt(prompt("What is your height in inches?"));
+
+//Separate Variables
+
+var kg = function(lbs) {
+	return lbs * 0.45;
+};
+var metersq = function(inches) {
+	return (inches * 0.025) ** 2;
+};
+//EVERYTHING ABOVE WORKS
+
+
+var bmi = function(kg, metersq) {
+	return kg(lbs) / metersq(inches);
+};
+
+alert("Your BMI is " + bmi(kg, metersq).toFixed(2));
+
+//Conversions all in one
+
+//var bmi = function(lbs, inches) {
+	//return (lbs * 0.45) / ((inches * 0.025) ** 2);
+//};
+
+//alert(bmi(lbs, inches));
+
+// Fahrenheit to Celsius (and vice versa) converter
+
+var celsius = parseInt(prompt("What is the temperature in Celsius?"));
+
+var fahrenheit = function(celsius) {
+	return celsius * (9/5) + 32;
+};
+
+alert(fahrenheit(celsius));
+
+var fahrenheit = parseInt(prompt("What is the temperature in Fahrenheit?"));
+
+var celsius = function(fahrenheit) {
+	return (fahrenheit - 32) * (5/9);
+};
+
+alert(celsius(fahrenheit));
