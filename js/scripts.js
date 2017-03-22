b879b90a0a007844f4fd5e9677494816863784c4
Grady Shelton and Spener Ruiz <grady@>
2017-03-22 09:49:40 -0700

    add bootstrap and jquery link in html

--- js/scripts.js
+++ js/scripts.js
@@ -27,25 +27,25 @@ alert(result);
 
 // BMI CALCULATOR
 
-var lbs = parseInt(prompt("What is your weight in pounds?"));
-var inches = parseInt(prompt("What is your height in inches?"));
-
-//Separate Variables
-
-var kg = function(lbs) {
-	return lbs * 0.45;
-};
-var metersq = function(inches) {
-	return (inches * 0.025) ** 2;
-};
-//EVERYTHING ABOVE WORKS
-
-
-var bmi = function(kg, metersq) {
-	return kg(lbs) / metersq(inches);
-};
-
-alert("Your BMI is " + bmi(kg, metersq).toFixed(2));
+// var lbs = parseInt(prompt("What is your weight in pounds?"));
+// var inches = parseInt(prompt("What is your height in inches?"));
+//
+// //Separate Variables
+//
+// var kg = function(lbs) {
+// 	return lbs * 0.45;
+// };
+// var metersq = function(inches) {
+// 	return (inches * 0.025) ** 2;
+// };
+// //EVERYTHING ABOVE WORKS
+//
+//
+// var bmi = function(kg, metersq) {
+// 	return kg(lbs) / metersq(inches);
+// };
+//
+// alert("Your BMI is " + bmi(kg, metersq).toFixed(2));
 
 //Conversions all in one
 
@@ -57,18 +57,18 @@ alert("Your BMI is " + bmi(kg, metersq).toFixed(2));
 
 // Fahrenheit to Celsius (and vice versa) converter
 
-var celsius = parseInt(prompt("What is the temperature in Celsius?"));
-
-var fahrenheit = function(celsius) {
-	return celsius * (9/5) + 32;
-};
-
-alert(fahrenheit(celsius));
-
-var fahrenheit = parseInt(prompt("What is the temperature in Fahrenheit?"));
-
-var celsius = function(fahrenheit) {
-	return (fahrenheit - 32) * (5/9);
-};
-
-alert(celsius(fahrenheit));
+// var celsius = parseInt(prompt("What is the temperature in Celsius?"));
+//
+// var fahrenheit = function(celsius) {
+// 	return celsius * (9/5) + 32;
+// };
+//
+// alert(fahrenheit(celsius));
+//
+// var fahrenheit = parseInt(prompt("What is the temperature in Fahrenheit?"));
+//
+// var celsius = function(fahrenheit) {
+// 	return (fahrenheit - 32) * (5/9);
+// };
+//
+// alert(celsius(fahrenheit));
